6bddb3dd9263dcb63e6c54e6214375d2616cde23
muskan02062000 <sharmamuskan262000@gmail.com>
2025-02-10 16:17:56 +0530

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,9 @@
-
+const mongoose = require('mongoose');
+ 
+const taskSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: { type: String, required: true, unique: true },
+  completed: { type: Boolean, default: false },
+});
+ 
+module.exports = mongoose.model('Task', taskSchema);
